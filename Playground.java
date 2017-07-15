83b3a4e4ce23d73ab6f4bf2ef8dff4e495c1b18c
Sean Deetlefs <sean.deetlefs@yisseoul.org>
2017-07-15 11:21:47 -0600

    Playground Java File
    This is the first file to use as a general testing ground for code.

---
+++ Playground.java
@@ -0,0 +1,109 @@
+
+import java.util.Scanner; //import the Scanner library package
+
+import java.util.Arrays; //import array utility library package
+
+public class Playground
+{
+    public static void main(String[] args)
+    {
+        /*
+        
+        System.out.println("Hello, World!\n");
+        
+        double x = 6.79;
+        
+        System.out.println(x);
+        
+        System.out.println();
+        
+        int y = (int) (Math.random() * 10);
+        
+        System.out.println(y);
+        
+        System.out.println();
+        
+        int total, n;
+        double average;
+        
+        total = 5;
+        n = 3;
+        
+        System.out.println("total = " + total + "; n = " + n + "\n");
+        
+        average = total/n; //integer division?
+        
+        System.out.println("average int division = " + average + "\n");
+        
+        average = (double) total/n; //total cast to double
+        
+        int trunc_test = (int) average; //casting an double to an int truncates the double.
+        
+        double int_to_double = n; //assigning an into to a double should produce a double
+        
+        System.out.println("average = " + average + "\n\n" + "trunc_test = " + trunc_test + "\n\n" + "int_to_double = " + int_to_double + "\n");
+        
+        //int max = Integer.MAX_VALUE;
+        
+        System.out.println("maximum integer value is " + Integer.MAX_VALUE + "\n");
+        
+        //int min = Integer.MIN_VALUE;
+        
+        System.out.println("minimum integer value is " + Integer.MIN_VALUE + "\n");
+        
+        //type double division by zero gives an infinitely large value but does not produce an exception!! See below:
+        
+        double zero = 0.0;
+        
+        double div_by_zero = average/zero;
+        
+        System.out.println(div_by_zero + "\n");
+        
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Enter a number: ");
+        int read_int = reader.nextInt(); // Scans the next token of the input as an int.
+        
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Enter a letter grade: ");
+        String grade = reader.nextLine(); // Scans the next line of the input as a String.
+        
+        if (grade.equals("A"))
+            System.out.println("Excellent!");
+        else if (grade.equals("B"))
+            System.out.println("Good");
+        else if (grade.equals("C"))
+            System.out.println("Poor");
+        else
+            System.out.println("Loser");
+        
+        int i;
+        
+        for (i = 1; i < 7; i++)     //basic for loop structure
+        {
+            System.out.println(i);
+        }
+         
+         
+        
+        int int_array[] = new int[5]; //initialize a new integer array of size 5 elements
+        
+        for (int j = 0; j < 5; j++)
+        {
+            int_array[j] = j+3;         //fill the array with values
+            
+            System.out.println(int_array[j]);   //print the array elements one at a time
+        }
+        
+        System.out.println(Arrays.toString(int_array));     //print the entire array using the array utilities package and Arrays.toString
+        
+        */
+        
+        int n;
+        
+        System.out.println(n=5);
+        
+        
+        
+        
+    }
+}
